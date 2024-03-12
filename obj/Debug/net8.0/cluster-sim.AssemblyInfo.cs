16ea78a02cc00e4a64e00f64a6a543b0dd4c577b
ggzdev <kian.schmalzl@icloud.com>
2024-03-12 15:43:10 +0100

    updated project file

--- obj/Debug/net8.0/cluster-sim.AssemblyInfo.cs
+++ obj/Debug/net8.0/cluster-sim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cluster-sim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f94be37efb67717de336f2a66a78e7dd42f2327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2396d47747a002ee200e416ffa0bdf2e0c9c1c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cluster-sim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cluster-sim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
