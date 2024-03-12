4f94be37efb67717de336f2a66a78e7dd42f2327
ggzdev <kian.schmalzl@icloud.com>
2024-03-12 15:38:11 +0100

    launcher updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// cluster-sim -- an open-source, highly customizable instrument cluster simulator
+// Copyright (c) 2024 Kian Schmalzl. All rights reserved. Licensed under MIT-License
+// make-file Version 0.1-stable [last update: 12.03.2024]
+// https://www.github.com/ggzdev/cluster-sim
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Numerics;
+
